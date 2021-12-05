cc91044f2dc9ef5909f8788fb692fe2bbc7f8855
MikeGu721 <59617922+MikeGu721@users.noreply.github.com>
2021-12-05 19:20:57 +0800

    Delete tools.py

--- tools.py
+++
@@ -1,9 +0,0 @@
-
-def LoadCSV(fn):
-    ret = []
-    with open(fn, encoding='utf-8') as fin:
-        for line in fin:
-            lln = line.rstrip('\r\n').split('\t')
-            ret.append(lln)
-    return ret
-
